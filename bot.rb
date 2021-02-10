8033771c927d0677b161fc7c98fb7475404ac6ae
Franck Siguier <franck.siguier@gmail.com>
2021-02-10 14:08:30 +0100

    Adding a counter to reach an end to the game

--- bot.rb
+++ bot.rb
@@ -9,6 +9,7 @@ BOT_TOKEN = ENV['BOT_TOKEN']
 BASE_URI = 'https://framex-dev.wadrid.net/api/video/Falcon%20Heavy%20Test%20Flight%20(Hosted%20Webcast)-wbSwFU6tY1c/frame/'
 TOTAL_FRAMES = 61_696 # for this example but this can be made dynamic
 STARTING_FRAME = TOTAL_FRAMES / 2
+counter = 1
 
 QUESTION = 'Can you tell me if the rocket has launched ?'
 
@@ -16,8 +17,8 @@ FILEPATH = 'data.csv'
 CSV_OPTIONS = { col_sep: ',', force_quotes: true, quote_char: '"' }.freeze
 
 CSV.open(FILEPATH, 'wb', CSV_OPTIONS) do |csv|
-  # csv << ['Frame', 'MinFrame', 'MaxFrame']
-  csv << [STARTING_FRAME.to_s, 0.to_s, TOTAL_FRAMES.to_s]
+  # csv << ['Frame', 'MinFrame', 'MaxFrame', 'Counter']
+  csv << [STARTING_FRAME.to_s, 0.to_s, TOTAL_FRAMES.to_s, counter.to_s]
 end
 
 def fetch_photo(frame, bot, message)
@@ -26,9 +27,13 @@ def fetch_photo(frame, bot, message)
   bot.api.send_photo(chat_id: message.chat.id, photo: BASE_URI + frame.to_s, caption: "'Yes' or 'No'")
 end
 
-def store_csv(frame, min_frame, max_frame)
+def endgame(frame, bot, message)
+  bot.api.send_photo(chat_id: message.chat.id, photo: BASE_URI + frame.to_s, caption: "Frame number #{frame} is close enough to being the exact frame the rocket takes off")
+end
+
+def store_csv(frame, min_frame, max_frame, counter)
   CSV.open(FILEPATH, 'wb', CSV_OPTIONS) do |csv|
-    csv << [frame.to_s, min_frame.to_s, max_frame.to_s]
+    csv << [frame.to_s, min_frame.to_s, max_frame.to_s, counter.to_s]
   end
 end
 
@@ -52,22 +57,28 @@ Telegram::Bot::Client.run(BOT_TOKEN) do |bot|
       bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
     when 'No'
       array = parse_csv
+      counter = array[3]
+      counter += 1
       current_frame = array[0] # We read the current frame from the csv file
+      endgame(current_frame, bot, message) if counter > 10
       min_frame = current_frame # We update the minimum frame possible for the launch
       max_frame = array[2]
       current_frame += ((max_frame - current_frame) / 2)
-      store_csv(current_frame, min_frame, max_frame)
+      store_csv(current_frame, min_frame, max_frame, counter)
       fetch_photo(current_frame, bot, message)
     when 'Yes'
       array = parse_csv
+      counter = array[3]
+      counter += 1
       current_frame = array[0]
+      endgame(current_frame, bot, message) if counter > 10
       min_frame = array[1]
-      max_frame = current_frame
+      max_frame = current_frame # We update the maximum frame possible for the launch
       current_frame -= ((current_frame - min_frame) / 2)
-      store_csv(current_frame, min_frame, max_frame)
+      store_csv(current_frame, min_frame, max_frame, counter)
       fetch_photo(current_frame, bot, message)
     else
-      bot.api.send_message(chat_id: message.chat.id, text: "You must answer with 'Yes' or 'No'")
+      bot.api.send_message(chat_id: message.chat.id, text: 'You can type /start to start the process again')
     end
   end
 end
